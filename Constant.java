4770e9e83957e206eb4e163f965fe4d891777214
surajlangute <surajlangute55@gmail.com>
2022-11-01 21:18:31 +0530

    Added program file

---
+++ Constant.java
@@ -0,0 +1,11 @@
+package day30;
+
+
+	public class Constant {
+
+		public static final double RATE_PER_KM = 10;
+		public static final double TIME_CHARGE_PER_MIN = 1;
+		public static final double MIN_FARE_AMT = 50;
+
+	}
+
